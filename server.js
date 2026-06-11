cf9f7a7e2353dc999c25913bb1947c4023412d6b
Juan Manuel López Arias <163903566+JuanmLopezA@users.noreply.github.com>
2026-06-11 11:08:54 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,23 +1,29 @@
-import express from "express";
-
+const express = require("express");
 const app = express();
+
 app.use(express.json());
 
 let datos = [];
 
+// test
 app.get("/", (req, res) => {
   res.send("OK servidor");
 });
 
+// endpoint API ✅
 app.post("/api", (req, res) => {
+  console.log("Recibido:", req.body);
   datos.push(req.body);
   res.json({ ok: true });
 });
 
+// ver datos ✅
 app.get("/data", (req, res) => {
   res.json(datos);
 });
 
-app.listen(3000, () => {
-  console.log("Servidor listo");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Servidor listo en puerto", PORT);
 });
